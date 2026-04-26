a40c3e8c5f036c67484831180e73d7a97320a968
laxmisunagar04 <laxmisanjaysunagar04@gmail.com>
2026-04-26 14:37:57 +0530

    fix for CI pipeline

--- test_app.py
+++ test_app.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import os
 
 def test_title():
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")   # important
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(options=options)
+    # 👇 use system chromedriver (GitHub provides it)
+    service = Service()
 
+    driver = webdriver.Chrome(service=service, options=options)
+
+    # 👇 works both locally + GitHub
     file_path = os.path.abspath("index.html")
     driver.get("file://" + file_path)
 
